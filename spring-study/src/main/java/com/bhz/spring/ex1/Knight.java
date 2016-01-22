e25ea5ca3c194d42a2ce3404518fe8f741a6a9cd
yaohai2000 <yaohai2000@yahoo.com>
2016-01-22 15:16:13 +0800

    Delete spring-study

--- spring-study/src/main/java/com/bhz/spring/ex1/Knight.java
+++
@@ -1,5 +0,0 @@
-package com.bhz.spring.ex1;
-
-public interface Knight {
-	public void embarkQuest();
-}
--- spring-study/src/main/java/com/bhz/spring/ex1/Quest.java
+++
@@ -1,5 +0,0 @@
-package com.bhz.spring.ex1;
-
-public interface Quest {
-	public void embark();
-}
